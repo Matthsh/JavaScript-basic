2e0776bbac7a6611da90d89bd17225f27d03650a
Matthsh <matthsh15@gmail.com>
2021-03-07 17:00:34 -0300

    second commit about JS

---
+++ 5-Comentarios.js
@@ -0,0 +1,6 @@
+console.log("Bem vindo titica")
+
+// Para adicionar um comentário de linha ao código
+/* Para adicionar um comentário de bloco ao código */
+
+//Acessando o código fonte da pagina e o arquivo JS eu posso acessar os comentários do código
